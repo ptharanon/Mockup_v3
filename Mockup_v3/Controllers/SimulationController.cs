44b83cc049b06261cd4ae270176d7b94a6294262
Daniel Romeril <dromeril@student.unimelb.edu.au>
2017-04-21 20:43:34 +1000

    Simulation controller updated.
    - Simulation controller should only need minor adjustments now.
    - Changed the generation of the input signals (sample time currently
    0.02)

--- Mockup_v3/Controllers/SimulationController.cs
+++ Mockup_v3/Controllers/SimulationController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Mockup_v3.Models;
+using System;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Mockup_v3.Controllers
 {
@@ -17,9 +16,19 @@ namespace Mockup_v3.Controllers
 
 
         [HttpPost]
-        public ActionResult Simulate(String test)
+        public ActionResult Simulate(String motor, String inputSpeed, String inputTorque, int size)
         {
-            return Json(new { testresult = test }, JsonRequestBehavior.AllowGet);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            double[][] speedCoordinates = js.Deserialize<double[][]>(inputSpeed);
+            double[][] torqueCoordinates = js.Deserialize<double[][]>(inputTorque);
+
+            SimulationResults results = new SimulationResults();
+            //List<List<double[]>> output = results.startSimulation(speedCoordinates, torqueCoordinates, size);
+
+            // Perform some manipulation on output here.
+
+            //return Json(new { output = output }, JsonRequestBehavior.AllowGet
+            return Json(new { motor = motor, inputSpeed = inputSpeed, inputTorque = inputTorque, size = size });
         }
     }
 }
